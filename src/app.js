23187f88f4f5ef33b2bd3dad868f6b751e5f3d3c
Franced3000 <franc.spata@gmail.com>
2024-06-20 16:49:32 +0200

    implementato cluster su app.js, modifica sugli script di package.json

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const process =require('process')
 const express = require('express');
 require('dotenv').config();
 
@@ -5,7 +8,20 @@ const userRouter = require('./routes/user.route');
 const bookRouter = require('./routes/book.route');
 const authRoutes = require('./routes/auth.route');
 
-const { sequelize } = require('../models/index'); // Consolidato
+const { sequelize } = require('../models/index'); 
+
+if (cluster.isMaster) {
+  console.log('Master' + process.pid+ ' is running');
+  
+  for(let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+  cluster.on( 'exit', (worker, code, signal) => {
+    console.log('worker '+ worker.process.pid+ 'died')
+    cluster.fork();
+  })
+
+} else {
 
 //const logger = require('./middleware/logger');
 
@@ -28,4 +44,11 @@ app.get('/', (req, res) => {
   res.send({ message: 'API di root ( / ) non implementata!' });
 });
 
+
 module.exports = app;
+
+const port = process.env.SERVER_PORT || 3000;
+app.listen(port, () => console.log(`Server in ascolto sulla porta: ${port}`));
+}
+
+
